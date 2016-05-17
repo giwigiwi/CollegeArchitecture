dda6993d306dfc136a14ee2315bde0c9c476b581
Klonium <klonium12@gmail.com>
2016-05-17 13:03:50 +0600

    PointPath 0.1

--- Architecture/Assets/PointPath.cs
+++ Architecture/Assets/PointPath.cs
@@ -4,6 +4,18 @@ using System.Collections;
 public class PointPath : MonoBehaviour {
 
     public GameObject[] pathLine;
+    public bool monitoring = false;
+
+    public void Update()
+    {
+        if (monitoring)
+        {
+            for (int i = 0; i < pathLine.Length; i++)
+                Debug.DrawLine(transform.position, pathLine[i].transform.position);
+        }
+
+
+    }
 
 
 }
