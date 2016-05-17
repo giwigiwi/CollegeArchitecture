790570c206d841f45dd950c2cf961f08cd2117ed
Klonium <klonium12@gmail.com>
2016-05-17 14:25:52 +0600

    Point path v0.2 name monitoring

--- Architecture/Assets/PointPath.cs
+++ Architecture/Assets/PointPath.cs
@@ -5,14 +5,28 @@ public class PointPath : MonoBehaviour {
 
     public GameObject[] pathLine;
     public bool monitoring = false;
+    public GameObject text;
+    private TextMesh textM;
+    private GameObject gmText;
+
+    public void Start()
+    {
+        gmText = (GameObject)Instantiate(text, transform.position, Quaternion.identity);
+        textM = gmText.GetComponent<TextMesh>();
+    }
 
     public void Update()
     {
         if (monitoring)
         {
+
             for (int i = 0; i < pathLine.Length; i++)
                 Debug.DrawLine(transform.position, pathLine[i].transform.position);
+            textM.text = gameObject.name;
         }
+
+
+        
 
 
     }
